569fb792158f91fa6143364a83a4c49fc84be5b3
tinchomalandra <martin.malandra@gmail.com>
2022-02-26 14:34:20 -0300

    correccion en el index.

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
 import mostrarProductos from "./components/products/mostrarProductos.js";
-import { stockProductos } from "./data/products.js"
+import { PRODUCTS } from "./data/products.js"
 
 document.addEventListener("DOMContentLoaded", (e) => {
     mostrarProductos(PRODUCTS);
